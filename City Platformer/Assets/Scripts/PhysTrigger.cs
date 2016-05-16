6c24e4954aab4c2e8c05e8c54e94c7385bbc382b
crazya02 <alekfleischer@yahoo.com>
2016-05-16 16:29:17 -0700

    Green box trigger now works. Also, BLOOM???

---
+++ City Platformer/Assets/Scripts/PhysTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter () {
+		Debug.Log ("Collided!");
+		gameObject.GetComponent<ConstantForce>().enabled = true;
+	}
+}
